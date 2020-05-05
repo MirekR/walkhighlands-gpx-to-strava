624ad7367170d6129fb1047342ab1070faa82cfe
mirekrousal@centrum.cz <mirekrousal@centrum.cz>
2020-05-05 21:29:54 +0100

    Works under zsh

--- toStrava.sh
+++ toStrava.sh
@@ -5,8 +5,8 @@ today=$(date +"%Y-%m-%d")
 
 cp ${gpx_source} ${gpx_strava}
 
-sed -i 's|<rte>|<trk><trkseg>|g' ${gpx_strava}
-sed -i 's|</rte>|</trkseg></trk>|g' ${gpx_strava}
+sed -i'.original' -e  's|<rte>|<trk><trkseg>|g' "${gpx_strava}"
+sed -i'.original' -e  's|</rte>|</trkseg></trk>|g' "${gpx_strava}"
 
-sed -i 's|<rtept|<trkpt|g' ${gpx_strava}
-sed -i "s|</rtept>|<time>${today}T07:07:07Z</time></trkpt>|g" ${gpx_strava}
+sed -i'.original' -e  's|<rtept|<trkpt|g' "${gpx_strava}"
+sed -i'.original' -e  "s|</rtept>|<time>${today}T07:07:07Z</time></trkpt>|g" "${gpx_strava}"
